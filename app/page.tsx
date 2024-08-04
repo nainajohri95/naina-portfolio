562787b1038272054f120d2750264246f8141d37
Naina Johri <jhorinaina8@gmail.com>
2024-08-04 22:41:38 +0530

    in tailwind.config personalised css added and in page.txt css added

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div>
+        <h1>Hello Portfolio</h1>
+      </div>
+    </main>
   );
 }
