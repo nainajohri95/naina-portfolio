a9c45eff88cadd62b8bdac6a00294273af1a9deb
Naina Johri <jhorinaina8@gmail.com>
2024-08-06 23:32:01 +0530

    changes made in Hero.tex file

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Spotlight } from './ui/Spotlight';
+import { TextGenerateEffect } from './ui/TextGenerateEffect';
 
 const Hero = () => {
   return (
@@ -17,9 +18,13 @@ const Hero = () => {
         </div>
         <div className='flex justify-center relative my-20 z-10'>
           <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            
             <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
               Dynamic web Magic with Next.js
             </h2>
+            <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl'
+            words = "Welcome to my Portfolio!"/>
+            <p className='text-center md:tracking-wide mb-4 text-sm md:text-lg lg:text-2xl'>Hi, I'm Naina Johri</p>
           </div>
         </div>
       </div>
